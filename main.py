dafda3439c50b028546eec1e76df3fa0229addb1
David González <dgonzalezdiez@ticketbis.com>
2015-09-26 10:14:48 +0200

    Upload to Amazon S3 and timestamp added

--- main.py
+++ main.py
@@ -3,7 +3,11 @@
 
 import xml.etree.ElementTree as ET
 import urllib2
+import ConfigParser
+import time
 from jinja2 import Template,FileSystemLoader,Environment
+from boto.s3.connection import S3Connection
+from boto.s3.key import Key
 
 class Incidence:
 	carretera = ''
@@ -27,6 +31,8 @@ class Incidence:
 	def print_as_dictionary(self):
 	  return {'carretera':carretera, 'fecha':fecha, 'km':km, 'sentido':sentido, 'nivel':nivel}
 
+fecha = time.strftime("%c")
+print fecha
 
 #tree = ET.parse('sample.xml')
 tree = ET.parse(urllib2.urlopen('http://www.trafikoa.net/servicios/IncidenciasTDT/IncidenciasTrafikoTDTGeo'))
@@ -46,5 +52,17 @@ templateEnv = Environment( loader=templateLoader )
 template = templateEnv.get_template( 'index.html' )
 
 index = open('index.html', 'w')
-index.write(template.render(incidencias=incidences).encode('utf-8'))  
+index.write(template.render(incidencias=incidences, fecha=fecha).encode('utf-8'))  
 index.close()
+
+#UPLOAD TO S3
+config = ConfigParser.ConfigParser()
+config.read("config.ini")
+AWS_ACCESS_KEY_ID=config.get("aws", "aws_access_key_id")
+AWS_SECRET_ACCESS_KEY=config.get("aws","aws_secret_access_key")
+
+conn = S3Connection(AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY)
+bucket = conn.get_bucket('traffic.penguinjournals.com')
+key = Key(bucket)
+key.key = 'index.html'
+key.set_contents_from_filename('index.html')
